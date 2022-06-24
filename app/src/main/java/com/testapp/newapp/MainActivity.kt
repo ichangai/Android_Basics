acf5a319b5082436d5b47160a4695b8ad7ebff9f
Ichangai <ichangaigitundu@gmail.com>
2022-06-24 08:48:16 +0300

    Using Logcat in Android Development

--- app/src/main/java/com/testapp/newapp/MainActivity.kt
+++ app/src/main/java/com/testapp/newapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.testapp.newapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.i("MYTAG", "MainActivity : Oncreate")
         val title = findViewById<TextView>(R.id.mainTitle)
         val inputField = findViewById<EditText>(R.id.userName)
         val submitBtn = findViewById<Button>(R.id.submitBtn)
@@ -25,10 +26,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "Please, enter your name!", Toast.LENGTH_SHORT)
             } else {
                 val msg = "Welcome home $username"
+                Log.i("MYTAG", msg)
                 title.text = msg
+                Log.i("MYTAG", "After displaying the message on the TextView")
                 inputField.text.clear()
                 offerBtn.visibility = VISIBLE
-            }
+            } 
         }
         offerBtn.setOnClickListener{
             val intent = Intent(this, MainActivity2::class.java)
